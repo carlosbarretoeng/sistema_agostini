0e98160cd5ba4e7eb3d0e94806b1be733664bd8d
Carlos Roberto Barreto Junior <carlosbarretoeng@users.noreply.github.com>
2022-08-14 01:25:02 +0000

    First Commit

---
+++ app/src/layouts/MainLayout.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="fas fa-bars"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Sistema Agostini
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      :width="200"
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+      </q-list>
+    </q-drawer>
+
+    <div class="q-pa-md">
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+    </div>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+});
+</script>
---
+++ app/src/pages/IndexPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-page>
+    <q-toolbar inset class="bg-grey-3 text-grey-7 q-px-md">
+      <span class="text-h4 text-weight-bold">
+        Companies
+      </span>
+      <q-space />
+      <q-btn flat round dense icon="fas fa-circle-plus" />
+    </q-toolbar>
+    <q-table
+      :loading="loading"
+      flat
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+      separator="cell"
+    />
+  </q-page>
+</template>
+
+<script lang="ts">
+import { api } from './../boot/axios'
+import { defineComponent, onMounted, ref } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup () {
+    const loading = ref(true)
+    const columns = ref([
+      { name: 'id', label: 'ID', field: 'id' },
+      { name: 'name', label: 'Name', field: 'name' },
+    ])
+    const rows = ref([])
+
+    onMounted(async () => {
+      const model = await api.get('/companies')
+      console.log(model);
+      rows.value = model.data
+      loading.value = false
+    })
+    return {
+      loading,
+      columns,
+      rows
+    };
+  }
+});
+</script>
